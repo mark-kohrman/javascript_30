a548daad3e4aa019b389d14487af89992c090773
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-18 20:40:12 -0500

    adding function of takePhoto to get iimage to show up at bottom

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -29,4 +29,18 @@ function paintToCanvas() {
   }, 16);
 }
 
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`
+  strip.insertBefore(link, strip.firsChild);
+}
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
